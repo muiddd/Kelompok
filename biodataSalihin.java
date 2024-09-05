0e6a5b22a4b019d02bb18ce01c51f58a05f256da
Abelassolihin <abelasssolihin@gmail.com>
2024-09-05 12:52:15 +0700

    Biodata Salihin

---
+++ biodataSalihin.java
@@ -0,0 +1,15 @@
+public class biodataSalihin {
+
+    public static void main(String[] args) {
+        System.out.println("------------------------------------------------");
+        System.out.println("-- Data Mahasiswa Jurusan Teknologi Informasi --");
+        System.out.println("------------------------------------------------");
+        System.out.println("Nama     : Abelas Solihin");
+        System.out.println("NIM      : 244107020052");
+        System.out.println("Kelas    : 1D");
+        System.out.println("No.Absen : 02");
+        System.out.println("Prodi    : D4 Teknik Informatika");
+        System.out.println("No.Hp    : 085604023352");
+        System.out.println("Hobi     : silat");
+    }
+}
