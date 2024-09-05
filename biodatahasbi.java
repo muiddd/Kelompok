da659ae7766f6e59a0954ebc01d6a7e89e4f829f
muhammad hasbi ashiddiqi <muhammadhasbiasidiki175@gmail.com>
2024-09-05 12:18:43 +0700

    biodata hasbi

---
+++ biodatahasbi.java
@@ -0,0 +1,16 @@
+public class biodatahasbi{
+    public static void main(String[] args){
+        System.out.println("---------------------------------------------------------");
+        System.out.println("-- Data Mahasiswa Jurusan Teknologi Informasi POLINEMA --");
+        System.out.println("---------------------------------------------------------");
+        System.out.println("Nama    : Muhammad Hasbi Ashiddiqi ");
+        System.out.println("NIM     : 244107020235 ");
+        System.out.println("Kelas   : 1 D" );
+        System.out.println("No Absen: 18 " );
+        System.out.println("Prodi   : Tekhnik Informatika" );
+        System.out.println("Alamat  : Gapok ,arjosari rejoso,Pasuruan");
+        System.out.println("No HP   : 087866973031");
+        System.out.println("Hobi    : berenang");
+        
+    }
+}
